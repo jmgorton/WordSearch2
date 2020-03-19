7ea9a54bab39799779d74d131a2e9c3a4f3ad1be
jmgorton <jgorton23@gmail.com>
2020-03-18 19:43:47 -0500

    updated readme

--- src/main/java/com/github/jmgorton/wordsearch/App.java
+++ src/main/java/com/github/jmgorton/wordsearch/App.java
@@ -13,26 +13,26 @@ public class App extends PuzzleSolver
     //    public InputStream input;
 
     public App(String filePath) {
-	//	this.filePath = filePath;
-	//	try {
-	//	    if (filePath != null) {
-	//		this.file = new File(filePath);
-	//	    }
-	//	    if (this.file != null && this.file.exists() && this.file.canRead()) {
-	//		this.input = new FileInputStream(this.file);
-	//	    }
-	//	} catch (Exception e) {
-	//	    e.printStackTrace();
-	//	} finally {
-	//	    if (this.input == null) {
-	//		this.input = System.in;
-	//	    }
-	//	}
+		//	this.filePath = filePath;
+		//	try {
+		//	    if (filePath != null) {
+		//		this.file = new File(filePath);
+		//	    }
+		//	    if (this.file != null && this.file.exists() && this.file.canRead()) {
+		//		this.input = new FileInputStream(this.file);
+		//	    }
+		//	} catch (Exception e) {
+		//	    e.printStackTrace();
+		//	} finally {
+		//	    if (this.input == null) {
+		//		this.input = System.in;
+		//	    }
+		//	}
 
-	///////////////////////////////////////
+		///////////////////////////////////////
 
-	super(filePath);
-	System.out.println("app filePath constructor");
+		super(filePath);
+		System.out.println("app filePath constructor");
 
     }
 
@@ -40,13 +40,13 @@ public class App extends PuzzleSolver
     {
         System.out.println( "Hello World!" );
 
-	for (int i = 0; i < args.length; i++) {
-	    System.out.println(args[0]);
-	}
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[0]);
+		}
 
-	//	PuzzleSolver ps = new PuzzleSolver();
-	//	ps.main(args);
+		//	PuzzleSolver ps = new PuzzleSolver();
+		//	ps.main(args);
 
-	PuzzleSolver.main(args);
+		PuzzleSolver.main(args);
     }
 }
